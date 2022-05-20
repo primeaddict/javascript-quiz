898d684cecaccf3a3e384cfc8d501793ecff41db
primeaddict <=>
2022-05-20 19:26:33 +0530

    added static page genration

--- javascript-quiz-frontend/pages/index.js
+++ javascript-quiz-frontend/pages/index.js
@@ -1,3 +1,4 @@
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -8,4 +9,10 @@ export default function Home() {
       Coming Soon...🎶
     </div>
   )
+}
+
+export async function getStaticProps(context) {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
 }
